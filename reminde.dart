810d7b27e8f1a93407dc953bbe312d73aed5d6e5
Chanidapha65310920 <iamfriendchi@gmail.com>
2024-11-18 15:15:10 +0700

    reminder.dart

---
+++ reminde.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  int num1 = 10;
+  int num2 = 3;
+
+ int quotient = (num1 / num2).toInt();
+ print("Output is ");
+ print("Quotient $quotient");
+
+ int remainder = num1 % num2;
+ print("Remainder $remainder");
+}
