1ae278dcc1d1f4153a3922036aa28b39f2725e2f
Chanidapha65310920 <iamfriendchi@gmail.com>
2024-11-18 16:56:15 +0700

    แก้ไขShare

--- share.dart
+++ share.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 void main() {
-  print("Enter Total bill amount:");
+  stdout.write("Enter Total bill amount:");
   double Formula = double.parse(stdin.readLineSync()!);
-  print("Enter num of People:");
+   stdout.write("Enter num of People:");
   int numPeople = int.parse(stdin.readLineSync()!); 
 
   double spiltAmount = Formula/numPeople;
-  print("You have to split amount of bill : ${spiltAmount}");
+   stdout.write("You have to split amount of bill : ${spiltAmount}");
 }
